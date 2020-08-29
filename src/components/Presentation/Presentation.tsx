e14a8b1dfdc6fd55e5183be06c5df913a2292715
Edson Cabral Junior <e2cabraljunior@gmail.com>
2020-08-29 12:14:25 -0300

    🚧 create a presentation component

---
+++ src/components/Presentation/Presentation.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import './index.css';
+
+export const Presentation = () => {
+  return (
+    <div className="page-header header-filter clear-filter purple-filter e2-bg-image" data-parallax="true">
+      <div className="container">
+        <div className="row">
+          <div className="col-md-8 ml-auto mr-auto">
+            <div className="brand">
+              <h1>Material Kit.</h1>
+              <h3>A Badass Bootstrap 4 UI Kit based on Material Design.</h3>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
