289bc4061d82f44556c863720f05ca2f4c47af3a
Edson Cabral Junior <e2cabraljunior@gmail.com>
2020-08-29 19:35:23 -0300

    🚧 import components to compose the page

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -2,6 +2,11 @@ import React from 'react';
 
 import { NavBar } from '../../components/NavBar/NavBar';
 import { Presentation } from '../../components/Presentation/Presentation';
+import { Main } from '../../components/Main/Main';
+import { About } from '../../components/About/About';
+import { Technologies } from '../../components/Technologies/Technologies';
+import { Projects } from '../../components/Projects/Projects';
+import { Footer } from '../../components/Footer/Footer';
 
 import '../../assets/css/global.css';
 
@@ -10,6 +15,12 @@ export const Home: React.FC = () => {
     <div>
       <NavBar />
       <Presentation />
+      <Main>
+        <About />
+        <Technologies />
+        <Projects />
+      </Main>
+      <Footer />
     </div>
   );
 }
