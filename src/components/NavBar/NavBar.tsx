f4a82ef09812afd4e477dbe4bab037a118d64173
Edson Cabral Junior <e2cabraljunior@gmail.com>
2020-08-29 19:34:57 -0300

    🚧 create a navbar component

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -5,8 +5,9 @@ export const NavBar: React.FC = () => {
     <nav className="navbar navbar-transparent navbar-color-on-scroll fixed-top navbar-expand-lg" color-on-scroll="100" id="sectionsNav">
       <div className="container">
         <div className="navbar-translate">
-          <a className="navbar-brand" href="#">
-            Material Kit </a>
+          <a className="navbar-brand" href="https://e2cabral.github.io">
+            Edson Cabral Junior
+          </a>
           <button className="navbar-toggler" type="button" data-toggle="collapse" aria-expanded="false" aria-label="Toggle navigation">
             <span className="sr-only">Toggle navigation</span>
             <span className="navbar-toggler-icon"></span>
@@ -16,42 +17,29 @@ export const NavBar: React.FC = () => {
         </div>
         <div className="collapse navbar-collapse">
           <ul className="navbar-nav ml-auto">
-            <li className="dropdown nav-item">
-              <a href="#" className="dropdown-toggle nav-link" data-toggle="dropdown">
-                <i className="material-icons">apps</i> Components
-              </a>
-              <div className="dropdown-menu dropdown-with-icons">
-                <a href="./index.html" className="dropdown-item">
-                  <i className="material-icons">layers</i> All Components
-                </a>
-                <a href="#" className="dropdown-item">
-                  <i className="material-icons">content_paste</i> Documentation
-                </a>
-              </div>
-            </li>
             <li className="nav-item">
-              <a className="nav-link" href="javascript:void(0)">
-                <i className="material-icons">cloud_download</i> Download
+              <a className="nav-link" rel="tooltip" title="" data-placement="bottom" href="https://github.com/e2cabral" target="_blank" data-original-title="Follow us on Twitter">
+                <i className="fa fa-github"></i>
               </a>
             </li>
+            {/* <li className="nav-item"> */}
+              {/* <a className="nav-link" rel="tooltip" title="" data-placement="bottom" href="https://twitter.com" target="_blank" data-original-title="Follow us on Twitter"> */}
+                {/* <i className="fa fa-twitter"></i> */}
+              {/* </a> */}
+            {/* </li> */}
+            {/* <li className="nav-item"> */}
+              {/* <a className="nav-link" rel="tooltip" title="" data-placement="bottom" href="https://www.facebook.com" target="_blank" data-original-title="Like us on Facebook"> */}
+                {/* <i className="fa fa-facebook-square"></i> */}
+              {/* </a> */}
+            {/* </li> */}
             <li className="nav-item">
-              <a className="nav-link" href="#" target="_blank">
-                <i className="material-icons">unarchive</i> Upgrade to PRO
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" rel="tooltip" title="" data-placement="bottom" href="https://twitter.com" target="_blank" data-original-title="Follow us on Twitter">
-                <i className="fa fa-twitter"></i>
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" rel="tooltip" title="" data-placement="bottom" href="https://www.facebook.com" target="_blank" data-original-title="Like us on Facebook">
-                <i className="fa fa-facebook-square"></i>
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" rel="tooltip" title="" data-placement="bottom" href="https://www.instagram.com/" target="_blank" data-original-title="Follow us on Instagram">
+              <a className="nav-link" rel="tooltip" title="" data-placement="bottom" href="https://www.instagram.com/odevjr/" target="_blank" data-original-title="Follow us on Instagram">
                 <i className="fa fa-instagram"></i>
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" rel="tooltip" title="" data-placement="bottom" href="https://www.linkedin.com/in/e2-cabral-junior/" target="_blank" data-original-title="Follow us on Instagram">
+                <i className="fa fa-linkedin"></i>
               </a>
             </li>
           </ul>
