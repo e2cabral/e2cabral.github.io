4ae3c4b3b403a3b35d93e7ab85cf1a3ba49b8041
Edson Cabral Junior <e2cabraljunior@gmail.com>
2020-08-29 19:33:40 -0300

    🚧 create a projects component

--- src/components/Presentation/Presentation.tsx
+++ src/components/Presentation/Presentation.tsx
@@ -4,13 +4,15 @@ import './index.css';
 
 export const Presentation = () => {
   return (
-    <div className="page-header header-filter clear-filter purple-filter e2-bg-image" data-parallax="true">
+    <div className="page-header header-filter clear-filter e2-bg-image" data-parallax="true">
       <div className="container">
         <div className="row">
           <div className="col-md-8 ml-auto mr-auto">
             <div className="brand">
-              <h1>Material Kit.</h1>
-              <h3>A Badass Bootstrap 4 UI Kit based on Material Design.</h3>
+              <h1>Edson <strong>Cabral</strong> Junior.</h1>
+              <h3>
+                Full Stack Node Developer
+              </h3>
             </div>
           </div>
         </div>
