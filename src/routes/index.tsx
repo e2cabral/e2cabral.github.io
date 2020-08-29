510c8615707553e100597c9b3cc803919255ab57
Edson Cabral Junior <e2cabraljunior@gmail.com>
2020-08-29 12:15:47 -0300

    ✨ add routes to project

---
+++ src/routes/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+
+import { Home } from '../pages/home/Home';
+
+export const Routes = () => {
+  return (
+    <BrowserRouter>
+      <Route path="/" exact component={Home} />
+    </BrowserRouter>
+  );
+}
