978493f6018f19e559b091a12c7abfede7240052
Edson Cabral Junior <e2cabraljunior@gmail.com>
2020-09-01 19:12:47 -0300

    🐛 fix a bug related to the column layout

--- e2cabral.github.io/src/components/About/About.tsx
+++ e2cabral.github.io/src/components/About/About.tsx
@@ -5,10 +5,10 @@ import { Title } from '../Title/Title';
 export const About = () => {
   return (
     <div className="row">
-      <div className="col-3">
-        <img src="https://avatars1.githubusercontent.com/u/19250015?s=460&u=3840bc12f0e6ad5d9468be17d12e3329b1a225d5&v=4" alt="Circle Image" className="img-raised rounded-circle img-fluid" />
+      <div className="col-md-3 col-sm-12 col-xs-12">
+        <img src="https://avatars1.githubusercontent.com/u/19250015?s=460&u=3840bc12f0e6ad5d9468be17d12e3329b1a225d5&v=4" alt="My github thumbnail" className="img-raised rounded-circle img-fluid" />
       </div>
-      <div className="col-9">
+      <div className="col-md-9 col-sm-12 col-xs-12">
         <Title title="Sobre mim" />
         <h4>
           Desenvolvedor Full Stack Node,
