b2a0b05740f9b8f88f6250d336a773b5671d5453
Edson Cabral <edson.cabral@trescon.com.br>
2020-09-04 20:21:24 -0300

    ✨ create qr-code-generator project

---
+++ qr-code-generator/precache-manifest.08db770f46f0841aaa99bf44c0d48111.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3723ee70d15b06a3d83dd42208c15f5f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "20a70871414b96f4564e",
+    "url": "/static/css/main.f4b39ca3.chunk.css"
+  },
+  {
+    "revision": "9656bb0bbdd8cefce6ea",
+    "url": "/static/js/2.33a8beec.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.33a8beec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "20a70871414b96f4564e",
+    "url": "/static/js/main.45a36454.chunk.js"
+  },
+  {
+    "revision": "3e6f82d29d9d51bdaf4a",
+    "url": "/static/js/runtime-main.47b14b55.js"
+  },
+  {
+    "revision": "0c34bb3d31f452c0495c221dbca5daa4",
+    "url": "/static/media/icofont.0c34bb3d.woff"
+  },
+  {
+    "revision": "ea7ee9403afa047019f69646d78c6b42",
+    "url": "/static/media/icofont.ea7ee940.woff2"
+  }
+]);
