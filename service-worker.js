9b0c7e09b7316770d7a684f0b96958ba0da2e5f0
Edson Cabral Junior <e2cabraljunior@gmail.com>
2020-12-31 19:21:50 -0300

    🚀 generate the final version of my profile

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.027d5a25e4adda5b575dbec847515079.js"
+  "/precache-manifest.c18d61e781b4b7fd724334b730ec68b1.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
