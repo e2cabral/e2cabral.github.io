e4cb450b7abd82b45b0724c98984f62f0779b039
Edson Cabral Junior <e2cabraljunior@gmail.com>
2020-12-31 18:47:48 -0300

    💬 add more description about me

--- e2cabral.github.io/src/components/About/About.tsx
+++ e2cabral.github.io/src/components/About/About.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 
 import { Title } from '../Title/Title';
 
+import './index.css';
+
 export const About = () => {
   return (
     <div className="row">
@@ -9,15 +11,28 @@ export const About = () => {
         <img src="https://avatars1.githubusercontent.com/u/19250015?s=460&u=3840bc12f0e6ad5d9468be17d12e3329b1a225d5&v=4" alt="My github thumbnail" className="img-raised rounded-circle img-fluid" />
       </div>
       <div className="col-md-9 col-sm-12 col-xs-12">
-        <Title title="Sobre mim" />
-        <h4>
-          Desenvolvedor Full Stack Node,
-          <strong className="tim-note"> apaixonado</strong> por planejar, escrever 
-          e entregar <strong className="tim-note">experiências incríveis </strong>
-          em formato de software. Proficiente 
-          no desenvolvimento de satisfação e 
-          adição de valor.
-        </h4>
+        <Title title="About me" />
+        <p className="f-size-24">
+          I'm a <strong className="tim-note">Fullstack Developer/DevOps</strong>
+          student quite curious and joining a ton of new programming languages.
+          I truly love to make a bunch of thing just to test new programming
+          languages, frameworks or libraries.
+        </p>
+        <p className="f-size-24">
+          I've been building a lot of things using
+          <strong className="tim-note"> Vue</strong>,
+          <strong className="tim-note"> React</strong>,
+          Angular, <strong className="tim-note">Typescript</strong>, Rust, Go,
+          C#, .NET Core, etc. However, my main stack is Node with Typescript,
+          and I'm also working with Go.
+        </p>
+        <p className="f-size-24">
+          I'm currently working at
+          <a href="https://www.iblueconsulting.com.br/" target="_blank"> iBlue Consulting </a>
+           as a <strong className="tim-note">Fullstack Node Developer</strong>.
+          I'm also trying to work on open-source projects, helping and learning
+          from the community.
+        </p>
       </div>
     </div>
   );
