049945172b1a10a35ca3fdefc7c09c4da8ab50c9
Edson Cabral Junior <e2cabraljunior@gmail.com>
2023-05-12 14:55:02 -0300

    chore: add them to the project data class

--- src/data/project/project-data.ts
+++ src/data/project/project-data.ts
@@ -2,10 +2,11 @@ import type { RouteParamValue } from 'vue-router'
 import { flueryInnovations, flueryRollout } from '@/data/project/fleury'
 import type { ProjectDetails } from '@/domain/types/project.type'
 import { cpqi9thGear, cpqiM7consigned } from '@/data/project/cpqi'
+import {darwin, doadin} from "@/data/project/iblue";
 
 export default class ProjectData {
   static findAll() {
-    return [flueryInnovations, flueryRollout, cpqi9thGear, cpqiM7consigned]
+    return [flueryInnovations, flueryRollout, cpqi9thGear, cpqiM7consigned, doadin, darwin]
   }
   static findByKeyOrDefault(key: string | RouteParamValue[]): ProjectDetails {
     return this.findAll().find((p) => key === p.key) || flueryInnovations
