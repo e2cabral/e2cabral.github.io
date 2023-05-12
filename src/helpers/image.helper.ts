61d74c67bebfb7a778013d721c7570ba4b1e7dbc
Edson Cabral Junior <e2cabraljunior@gmail.com>
2023-05-12 14:02:40 -0300

    chore: add a new image file

--- src/helpers/image.helper.ts
+++ src/helpers/image.helper.ts
@@ -3,6 +3,7 @@ import type { Roles } from '@/domain/types/project.type'
 import astronaut from '@/assets/img/astronauta.jpg'
 import rocket from '@/assets/img/rocket.jpg'
 import library from '@/assets/img/library.jpg'
+import smartphone from '@/assets/img/smartphone.jpg'
 
 export const getImageByRoleOrDefault = (role: Roles) => {
   switch (role) {
@@ -14,6 +15,9 @@ export const getImageByRoleOrDefault = (role: Roles) => {
 
     case 'Engineering':
       return rocket
+
+    case 'Mobile':
+      return smartphone
 
     default:
       return astronaut
